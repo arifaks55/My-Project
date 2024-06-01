23ff901f01286864ef52ef2ea8f779ecb8eebb1c
arifaks55 <163207675+arifaks55@users.noreply.github.com>
2024-06-01 18:46:21 +0300

    JS-6878

---
+++ JS-6878/js/script.js
@@ -0,0 +1,19 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
+
+document.getElementById("tab1-button").style.backgroundColor = "lightblue";
+
+document.getElementById("tab2-button").style.backgroundColor = "lightgreen";
+
+document.getElementById("tab3-button").style.backgroundColor = "lightyellow";
